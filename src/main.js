c3138cdb48a71a7520b57e849b1a7959f1f1d492
Owen Jones <o-w-jones@outlook.com>
2018-04-23 22:44:44 +0100

    Create CountryList component and routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
+import CountryList from './CountryList.vue';
+
+const routes = {
+    '/': App,
+    '/country-list': CountryList,
+};
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    render: h => h(App),
+    data: {
+        currentRoute: window.location.pathname,
+    },
+    computed: {
+        ViewComponent() {
+            return routes[this.currentRoute] || App;
+        },
+    },
+    render(h) { return h(this.ViewComponent); },
 });
