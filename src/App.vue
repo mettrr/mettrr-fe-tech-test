21f68476c86dfeb7f56f838aad3fdbcf0bd2d22a
Daiki Obara <daikiobara@test.lan>
2018-04-26 00:04:39 +0100

    [add] Header component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
     <div id="app">
+        <!-- <app-header />
         <h1>{{ msg }}</h1>
         <img src="./assets/mettrr-logo.png">
+        <router-view/> -->
+        <v-app>
+            <main>
+                <v-container fluid>
+                    <app-header></app-header>
+                    <router-view/>
+                </v-container>
+            </main>
+        </v-app>
     </div>
+
 </template>
 
 <script>
-    export default {
-        name: 'MettrrTest',
-        data () {
-            return {
-                msg: 'Mettrr Tech Test'
-            }
-        } 
-    }
+import Header from './components/Header.vue';
+
+export default {
+    components: {
+        appHeader: Header,
+    },
+    name: 'MettrrTest',
+    data() {
+        return {
+            msg: 'Mettrr Tech Test',
+        };
+    },
+};
 </script>
 
 <style lang="scss">
     body {
         font-family: 'Nunito Sans', sans-serif;
         #app {
-            margin: 0 auto;
-            width: 300px;
-            text-align: center;
-            img {
-                width: 300px;
-            }
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
         }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import vSelect from 'vue-select';
+import App from './App.vue';
+import router from './router';
 
+Vue.use(Vuetify);
+Vue.component('v-select', vSelect);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router,
+    render: h => h(App),
+});
