be7d58f2f9b6a10d3aa32e2750b4306baa5ad894
Daiki Obara <daikiobara@test.lan>
2018-04-26 00:05:06 +0100

    [add] home component

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="container">
+            <h1>{{ msg }}</h1>
+            <img src="../assets/mettrr-logo.png"> 
+        </div> 
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            msg: 'Mettrr Tech Test',
+        };
+    },
+};
+
+</script>
+
+<style lang="scss">
+    .container {
+        margin-top: 60px;
+        width: 100%;
+        h1 {
+            text-align: center;
+            font-family: 'Avenir', Helvetica, Arial, sans-serif;
+            font-size: 36px;
+
+        }
+        img {
+            width: 100%;
+            max-width: 300px;
+            display: block;
+            margin: 0px auto;
+        }
+    }
+</style>
