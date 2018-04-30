0954b59c61a523b5521e142b456742699ef704a3
Jacob <jacobchen17@googlemail.com>
2018-04-30 18:31:47 +0100

    Create CountryList component and render country list

---
+++ src/components/CountryList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2>Country List</h2>
+        <router-link to="/">Home</router-link>
+        <ol>
+            <li v-for="country in countryList">
+                {{ country.name }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+import getCountryList from '../data/getCountryList';
+
+export default {
+    data() {
+        return {
+            countryList: [],
+        };
+    },
+    mounted() {
+        const self = this;
+        getCountryList().then((res) => {
+            self.countryList = res.data;
+        }).catch((err) => {
+
+        });
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
