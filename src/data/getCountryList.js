34c2ab61ea5c38050b2c379e319ecdcfd900ef66
Jacob <jacobchen17@googlemail.com>
2018-04-30 19:51:39 +0100

    Update getCountryList to return response.data

--- src/data/getCountryList.js
+++ src/data/getCountryList.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => new Promise((resolve, reject) => {
     axios.get('https://api.myjson.com/bins/16i597').then((response) => {
-        resolve(response);
+        resolve(response.data);
     }).catch((err) => {
         reject(err);
     });
