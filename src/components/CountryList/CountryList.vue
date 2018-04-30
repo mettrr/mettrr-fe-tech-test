f0207a716a416dafdc8fc3ea24e5920b95aef893
Jacob <jacobchen17@googlemail.com>
2018-04-30 22:06:33 +0100

    Add a loading spinner

--- src/components/CountryList/CountryList.vue
+++ src/components/CountryList/CountryList.vue
@@ -2,21 +2,29 @@
     <div>
         <h2>Country List</h2>
         <router-link to="/">Home</router-link>
-        <div class="dropdown">
-            <div class="dropdown-selected-item" 
-                v-on:click="toggleDropdownList">
-                <p v-if="selectedListItem">{{ selectedListItem.name }}</p>
+        <div v-if="!hasError" class="panel-container">
+            <div class="dropdown">
+                <div class="dropdown-selected-item" 
+                    v-on:click="toggleDropdownList">
+                    <p v-if="selectedListItem">{{ selectedListItem.name }}</p>
+                    <div v-if="isLoading" class="lds-dual-ring"></div>
+                </div>
+                <ul class="dropdown-list"
+                    v-bind:class="{active: dropdownListIsActive}">
+                    <li v-for="(country, index) in countryList" 
+                        :key="`fruit=${index}`"
+                        v-on:click="setSelectedListItem(country)">
+                        {{ country.name }}
+                    </li>
+                </ul>
             </div>
-            <ul class="dropdown-list"
-                v-bind:class="{active: dropdownListIsActive}">
-                <li v-for="(country, index) in countryList" 
-                    :key="`fruit=${index}`"
-                    v-on:click="setSelectedListItem(country)">
-                    {{ country.name }}
-                </li>
-            </ul>
+            <h3 v-if="selectedListItem">You selected {{selectedListItem.name}}</h3>
         </div>
-        <h3 v-if="selectedListItem">You selected {{selectedListItem.name}}</h3>
+        <div v-else class="panel-container">
+            Uh oh an error occured.
+            {{errorMessage}}
+            <button v-on:click="getCountryItems()">Retry?</button>
+        </div>
     </div>
 </template>
 
@@ -29,6 +37,7 @@ export default {
             countryList: [],
             selectedListItem: false,
             dropdownListIsActive: false,
+            isLoading: true,
             hasError: false,
             errorMessage: '',
         };
@@ -48,13 +57,16 @@ export default {
             const self = this;
 
             // Reset feedback indicators
+            self.isLoading = true;
             self.hasError = false;
             self.errorMessage = '';
 
             getCountryListExceptNoFlyList().then((list) => {
+                self.isLoading = false;
                 self.countryList = list;
                 [self.selectedListItem = 0] = list;
             }).catch((err) => {
+                self.isLoading = false;
                 self.hasError = true;
                 self.errorMessage = err.message;
             });
@@ -64,6 +76,9 @@ export default {
 </script>
 
 <style lang="scss">
+    .panel-container {
+        margin: 15px 0;
+    }
     .dropdown {
         position: relative;
         margin-top: 15px;
@@ -119,5 +134,29 @@ export default {
                 }
             }
         }   
+    }
+    .lds-dual-ring {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+    }
+    .lds-dual-ring:after {
+    content: " ";
+        display: block;
+        width: 12px;
+        height: 12px;
+        margin: 1px;
+        border-radius: 50%;
+        border: 5px solid rgba(117, 81, 255, 0.7);
+        border-color: rgba(117, 81, 255, 0.7) transparent rgba(117, 81, 255, 0.7) transparent;
+        animation: lds-dual-ring 1.2s linear infinite;
+    }
+    @keyframes lds-dual-ring {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
     }
 </style>
