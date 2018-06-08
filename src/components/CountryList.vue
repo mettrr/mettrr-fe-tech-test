d3bf5c3e36024ace01127a3c9a7796ffce938898
Sean <sean.rivard.morton@gmail.com>
2018-06-07 20:26:08 -0400

    Vue-Router set-up

---
+++ src/components/CountryList.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Test</h1>
+</template>
+
+<script>
+    export default {
+        name: 'CountryList',
+        data() {
+            return {
+                name: 'asdf',
+            };
+        },
+    };
+</script>
