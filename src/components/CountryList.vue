2d3f7bbfe8c2f1ab737562650f22088928b773af
Sean <sean.rivard.morton@gmail.com>
2018-06-07 22:32:20 -0400

    Added styled 'drop-down'

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,16 +1,18 @@
 <template>
-    <h1>Test</h1>
+    <auto-complete v-if="countryData.length > 0" :suggestions="allowedCountryList" v-model="selection" />
 </template>
 
 <script>
     import { HTTP } from '../plugins/axios';
     import NO_FLY_LIST from '../assets/noFlyList.json';
+    import AutoComplete from '../components/BasicComponents/AutoComplete.vue';
 
     export default {
         name: 'CountryList',
+        components: { AutoComplete },
         data() {
             return {
-                name: 'asdf',
+                selection: '',
                 countryData: [],
             };
         },
