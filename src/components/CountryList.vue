b6f8766162c3debce716590a481b4772f74672f5
Sean <sean.rivard.morton@gmail.com>
2018-06-07 20:44:55 -0400

    Added Axios & get CountryData

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -3,12 +3,34 @@
 </template>
 
 <script>
+    import { HTTP } from '../plugins/axios';
+    import NO_FLY_LIST from '../assets/noFlyList.json';
+
     export default {
         name: 'CountryList',
         data() {
             return {
                 name: 'asdf',
+                countryData: [],
             };
+        },
+        created() {
+            HTTP
+                .get('/bins/16i597')
+                .then((response) => {
+                    this.countryData = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        computed: {
+            allowedCountryList() {
+                return this.countryData
+                    .filter((country) => !NO_FLY_LIST.includes(country.name))
+                    .map(item => item);
+            },
         },
     };
 </script>
+
