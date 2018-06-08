d66c1afd6b5b41dc3cb60e10ef6eb9895c17bba9
Sean <sean.rivard.morton@gmail.com>
2018-06-07 22:38:44 -0400

    Fixed '#' in url

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import CountryList from '../components/CountryList.vue';
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '/countryList',
