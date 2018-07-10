0f6422518031372d4a857fa67f6e3b1e4527dfbc
unknown <beauagst@gmail.com>
2018-07-10 21:55:39 +0100

    feature: components Grabbed no-fly list Created loading spinner component Created dropdown component CountryList component now grabs data and passes to child component

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,29 +1,67 @@
 <template>
-    <h1>Country List</h1>
+    <div class="country-list">
+        <h1>Country List</h1>
+
+        <div class="country-list--container">
+            <LoadingSpinner
+                :loading="loading"
+            />
+            <DropDown
+                countries="filteredCountries"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
+import LoadingSpinner from './LoadingSpinner.vue';
+import DropDown from './DropDown.vue';
 import axios from '../api';
+import noFlyList from '../lib/noFlyList';
 
 export default {
     name: 'CountryList',
+    components: {
+        LoadingSpinner,
+        DropDown,
+    },
     data() {
         return {
             countryList: [],
+            loading: false,
+            error: false,
         };
     },
     created() {
-        axios.get('/16i597')
-            .then((data) => {
-
-            })
-            .catch((err) => {
-
-            });
+        this.getCountryList();
+    },
+    methods: {
+        getCountryList() {
+            this.loading = true;
+            axios.get('/16i597')
+                .then((res) => {
+                    this.countryList = res.data;
+                })
+                .catch((err) => {
+                    this.error = true;
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+    },
+    computed: {
+        filteredCountries() {
+            return this.countryList
+                .filter(({ name }) => !noFlyList.includes(name))
+                .map(country => country.name);
+        },
     },
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.country-list {
+    position: relative;
+}
 </style>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,82 @@
+<template>
+    <transition name="fade">
+        <div
+            class="loader"
+            v-if="loading"
+        ></div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'LoadingSpinner',
+    props: {
+        loading: {
+            type: Boolean,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.loader,
+.loader:before,
+.loader:after {
+    border-radius: 50%;
+}
+
+.loader {
+    color: #7551ff;
+    margin: 0 auto;
+    position: relative;
+    width: 4em;
+    height: 4em;
+    box-shadow: inset 0 0 0 .5em;
+    transform: translateZ(0);
+
+    &:before,
+    &:after {
+        content: '';
+        position: absolute;
+        top: -0.1em;
+        left: -0.1em;
+        width: 2.1em;
+        height: 4.1em;
+        background: #fff;
+        border-radius: 4em 0 0 4em;
+        transform-origin: 2.1em 2.1em;
+        animation: load 2s infinite ease 1.5s;
+    }
+
+    &:after {
+        background: #fff;
+        border-radius: 0 4em 4em 0;
+        left: 2.1em;
+        transform-origin: 0px 2.1em;
+        animation: load 2s infinite ease;
+    }
+}
+
+
+@keyframes load {
+    0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+    }
+}
+
+</style>
